faa18ad9f0442a97d466ee2cf03be046d43bf27f
Taghi Aliyev <taghialiyev@gmail.com>
2015-11-03 14:23:10 +0100

    Updating the copyright with DOI citing number

--- src/bicat/util/Option.java
+++ src/bicat/util/Option.java
@@ -1,8 +1,10 @@
 /*
- *                                BBiCat is a toolbox that combines different Biological Post-Analytic tools, Bi-Clustering and
- *                                clustering techniques in it, which can be applied on a given dataset. This software is the
- *                                modified version of the original BiCat Toolbox implemented at ETH Zurich by Simon
- *                                Barkow, Eckart Zitzler, Stefan Bleuler, Amela Prelic and Don Frick.
+ *                                BBiCat is a toolbox that combines different Bi-Clustering and clustering techniques in it, which
+ *                                can be applied on a given dataset. This software is the modified version of the original BiCat
+ *                                Toolbox implemented at ETH Zurich by Simon Barkow, Eckart Zitzler, Stefan Bleuler, Amela
+ *                                Prelic and Don Frick.
+ *
+ *                                DOI for citing the release : 10.5281/zenodo.33117
  *
  *                                Copyright (c) 2015 Taghi Aliyev
  *
