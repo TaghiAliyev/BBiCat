a7189e1f39b31c29374a20b21c6f0272f4bb51b2
Taghi Aliyev <taghialiyev@gmail.com>
2015-11-25 13:37:38 +0100

    KEGG implementation done. Parsing is rather slow now. Optimization is next on the list.

--- src/MetaFramework/KEGG/KeggPathway.java
+++ src/MetaFramework/KEGG/KeggPathway.java
@@ -71,6 +71,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Class that parses and analyzes biclusters based on the KEGG Pathway lists
@@ -83,8 +84,11 @@ import java.util.ArrayList;
 public class KeggPathway extends AbstractPathwayAnalysis<KEGGMolecule, KEGGInteraction, KEGGPathwayInstance>{
 
     private String allKeggPath = "http://rest.kegg.jp/list/pathway/hsa";
+    private String allGenes = "http://rest.kegg.jp/list/hsa";
     private String getBaseLink = "http://rest.kegg.jp/get/";
     private String getGeneBase = "http://rest.kegg.jp/get/hsa:";
+
+    private HashMap<String, ArrayList<String>> codeToName = new HashMap<String, ArrayList<String>>();
 
     private URL allPathURL;
 
@@ -92,14 +96,54 @@ public class KeggPathway extends AbstractPathwayAnalysis<KEGGMolecule, KEGGInter
     {
         super(file);
         allPathURL = new URL(allKeggPath);
+        readAllGenes();
+        System.out.println("All genes are been mapped");
         parse();
+        System.out.println("All the pathways are done");
+    }
+
+    public void readAllGenes() throws Exception
+    {
+        URL allGeneURL = new URL(allGenes);
+        BufferedReader geneIn = new BufferedReader(new InputStreamReader(allGeneURL.openStream()));
+
+        String gene;
+        while ((gene = geneIn.readLine()) != null)
+        {
+            String[] parts = gene.split("\t");
+            String code = parts[0];
+            String geneName = parts[1];
+            String geneParts = geneName.split(";")[0];
+            String[] actualParts = geneParts.split(",");
+            ArrayList<String> toAddList = new ArrayList<String>();
+            for (int i = 0 ; i < actualParts.length; i++)
+            {
+                String toAdd;
+                if (actualParts.length != 1)
+                    toAdd = actualParts[i].replaceAll("\\s+", "");
+                else
+                    toAdd = actualParts[i];
+                toAddList.add(toAdd);
+//                System.out.println("Parts: " + toAdd + " , " + code);
+            }
+//            System.out.println("Gene with code : " + code + " has " + actualParts.length + " parts");
+            codeToName.put(code, toAddList);
+        }
+
     }
 
 
     public static void main(String[] args) throws Exception
     {
+        long start = System.currentTimeMillis();
         KeggPathway engine = new KeggPathway("");
-
+        long end = System.currentTimeMillis();
+        System.out.println("It took " + (end - start) + " ms to parse the KEGG");
+        ArrayList<KEGGPathwayInstance> mols = engine.getGeneToPath().get(new KEGGMolecule(0, "XDH", false, null));
+        for (KEGGPathwayInstance tmp : mols)
+        {
+            System.out.println("Molecule name in the pathway : " + tmp.getName());
+        }
     }
 
     @Override
@@ -110,10 +154,13 @@ public class KeggPathway extends AbstractPathwayAnalysis<KEGGMolecule, KEGGInter
         int pathCnt = 0, molCnt = 0;
         while((line = in.readLine()) != null)
         {
+            if (pathCnt % 50 == 0)
+                System.out.println(pathCnt);
             // Line is already read
             String[] parts = line.split("\t");
             String pathCode = parts[0];
             String pathName = parts[1].substring(0, parts[1].length() - 23);
+//            System.out.println(pathName);
             KEGGPathwayInstance path = new KEGGPathwayInstance(null, pathCnt, null, pathName);
 //            System.out.println("Path name : " + pathName);
             String pathLink = getBaseLink + pathCode;
@@ -125,21 +172,46 @@ public class KeggPathway extends AbstractPathwayAnalysis<KEGGMolecule, KEGGInter
             ArrayList<KEGGMolecule> mols = new ArrayList<KEGGMolecule>();
             while ((pLine = pathIn.readLine()) != null && (!geneStarted || !geneEnd))
             {
-                if (!pLine.substring(0, 4).equalsIgnoreCase("    ") && geneStarted)
+                if (pLine.length() < 4)
+                    System.out.println(pathName + " ;" + pathCode);
+                if (pLine.length() >= 4 && !pLine.substring(0, 4).equalsIgnoreCase("    ") && geneStarted)
                     geneEnd = true;
-                if (pLine.substring(0,4).equalsIgnoreCase("GENE"))
+                if (pLine.length() >= 4 && pLine.substring(0,4).equalsIgnoreCase("GENE"))
                     geneStarted = true;
                 if (geneStarted && !geneEnd) {
                     String pureGene = pLine.substring(12);
-                    String geneName = pureGene.split(" ")[2].split(";")[0];
-                    // TODO: LOOK INTO HOW COMPARISONS ARE DONE FOR HASHMAP THING.
-                    // THIS WAY ID WILL BE PROBLEMATIC
-                    KEGGMolecule toAdd = new KEGGMolecule();
-                    molCnt++;
-//                    System.out.println(geneName);
+                    // Getting the name and the code of the gene. Name might not be necessary
+                    String code = pureGene.split(" ")[0];
+                    if (pureGene.split(" ").length >= 3) {
+                        String geneName2 = pureGene.split(" ")[2].split(";")[0];
+                        ArrayList<String> geneName = codeToName.get("hsa:" + code);
+                        if (geneName == null)
+                            System.out.println(code + " ; " + geneName2);
+                        ArrayList<KEGGMolecule> pathMols = new ArrayList<KEGGMolecule>();
+                        // TODO: LOOK INTO HOW COMPARISONS ARE DONE FOR HASHMAP THING.
+                        // THIS WAY ID WILL BE PROBLEMATIC
+                        for (String tmp : geneName) {
+                            KEGGMolecule tmpMol = new KEGGMolecule(molCnt, tmp, false, null);
+                            mols.add(tmpMol);
+                            ArrayList<KEGGPathwayInstance> paths = this.geneToPath.get(tmpMol);
+                            if (paths == null) {
+                                paths = new ArrayList<KEGGPathwayInstance>();
+                                paths.add(path);
+                                this.geneToPath.put(tmpMol, paths);
+                            } else {
+                                paths.add(path);
+                                this.geneToPath.put(tmpMol, paths);
+                            }
+                        }
+
+                        molCnt++;
+//                    System.out.println(geneName.get(0) + " " + code);
+                    }
                 }
             }
-            System.out.println();
+            pathIn.close();
+//            System.out.println();
+            this.pathToGene.put(path, mols);
 //            System.out.println("Amount of parts : " + parts.length);
 //            System.out.println("First part : " + parts[0]);
 //            System.out.println("Second part : " + parts[1]);
